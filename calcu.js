e9ebdba59be9bc9f9af2ec1545ed9f6d08beda98
hitrish <mohdrishin04@gmail.com>
2023-08-21 14:58:58 +0530

    calculator

---
+++ calcu.js
@@ -0,0 +1,14 @@
+function display(content){
+    txt.value += content
+}
+
+function allClear(){
+    result.value=""
+}
+
+function exprEval(){
+    result.value = eval(result.value)
+}
+function disu(con){
+    txt.value += con
+}
